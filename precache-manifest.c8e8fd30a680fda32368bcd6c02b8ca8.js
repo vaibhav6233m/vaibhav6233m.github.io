c8fa7a493772892e01e440358d27dbb39b1bb6d6
Vaibhav Bhavsar <vaibhavb@winjit.com>
2019-01-09 20:08:33 +0530

    test app

---
+++ precache-manifest.c8e8fd30a680fda32368bcd6c02b8ca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "def6cf58e5475e9fc646",
+    "url": "/static/js/main.def6cf58.chunk.js"
+  },
+  {
+    "revision": "66cc806f992ce454ac0d",
+    "url": "/static/js/1.66cc806f.chunk.js"
+  },
+  {
+    "revision": "def6cf58e5475e9fc646",
+    "url": "/static/css/main.f87deffa.chunk.css"
+  },
+  {
+    "revision": "14190da8d493827b6e5b9f55fc2110d7",
+    "url": "/index.html"
+  }
+];
